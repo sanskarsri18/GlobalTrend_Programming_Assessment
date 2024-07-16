e86b8e44d07ffe86a03ae8dbc2a17ded8668c4f6
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-07-16 17:35:10 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,1 +1,32 @@
+// Write a function to compute the sum of the digits of a given number.The function should return the sum of the digits of the number.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to get sum of digits */
+class gfg {
+public:
+	int getSum(int n)
+	{
+		int sum;
+
+		/* Single line that calculates sum */
+		for (sum = 0; n > 0; sum += n % 10, n /= 10)
+			;
+
+		return sum;
+	}
+};
+
+// Driver code
+int main()
+{
+	gfg g;
+	int n = 687;
+
+	// Function call
+	cout << g.getSum(n);
+	return 0;
+}
+
 
