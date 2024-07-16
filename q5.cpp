c7c4fba02c8566bfdf8d08da75e133108b79450e
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-07-16 17:30:54 +0530

    Update q5.cpp

--- q5.cpp
+++ q5.cpp
@@ -1,1 +1,47 @@
+// Write a function to rotate an array to the right by k steps.The function should modify the array in place to achieve the rotation.
+
+// C++ implementation of right rotation 
+// of an array K number of times
+#include<bits/stdc++.h>
+using namespace std;
+
+// Function to rightRotate array
+void RightRotate(int a[], int n, int k)
+{
+    
+    // If rotation is greater 
+    // than size of array
+    k = k % n;
+
+    for(int i = 0; i < n; i++)
+    {
+       if(i < k)
+       {
+           
+           // Printing rightmost 
+           // kth elements
+           cout << a[n + i - k] << " ";
+       }
+       else
+       {
+           
+           // Prints array after
+           // 'k' elements
+           cout << (a[i - k]) << " ";
+       }
+    }
+    cout << "\n";
+}
+    
+// Driver code
+int main()
+{
+    int Array[] = { 1, 2, 3, 4, 5 };
+    int N = sizeof(Array) / sizeof(Array[0]);
+    int K = 2;
+    
+    RightRotate(Array, N, K);
+}
+
+
 
